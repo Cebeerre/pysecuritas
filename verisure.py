ee8c0c1cb6d00e4f53fcefaf28129ecb16bbd4b1
Cebeerre <cebeerre@hotmail.com>
2020-05-13 22:58:40 +0200

    New command IMG

--- verisure.py
+++ verisure.py
@@ -8,6 +8,7 @@ import json
 import argparse
 import textwrap
 import itertools
+import base64
 
 
 class VerisureAPIClient():
@@ -20,7 +21,8 @@ class VerisureAPIClient():
         'DARM': 'disarm everything (not the annex)',
         'ARMANNEX': 'arm the secondary alarm',
         'DARMANNEX': 'disarm the secondary alarm',
-        'EST': 'return the panel status'
+        'EST': 'return the panel status',
+        'IMG': 'Take a picture (requires -s)'
     }
     DAPI_OPS = {
         'ACT_V2': 'get the activity log',
@@ -35,6 +37,7 @@ class VerisureAPIClient():
 
     def __init__(self, **args):
         self.user = args.get('username')
+        self.sensor = args.get('sensor')
         self.LOGIN_PAYLOAD = {'Country': args.get('country'),
                               'user': args.get('username'), 'pwd': args.get('password'), 'lang': args.get('language')}
         self.OP_PAYLOAD = {'Country': args.get('country'), 'user': args.get('username'),
@@ -62,6 +65,12 @@ class VerisureAPIClient():
     def op_verisure(self, action, hash, id):
         payload = self.OP_PAYLOAD
         payload.update({'request': action, 'hash': hash, 'ID': id})
+        if action == 'IMG':
+            payload.update(
+                {'device': self.sensor, 'instibs': self.instibs, 'idservice': '1'})
+        if action == 'INF':
+            payload.update({'idsignal': self.idsignal,
+                            'signaltype': self.signaltype})
         if action in self.ALARM_OPS:
             payload['request'] = action + '1'
             self.call_verisure_get('GET', payload)
@@ -71,7 +80,7 @@ class VerisureAPIClient():
             while res != 'OK':
                 output = self.call_verisure_get('GET', payload)
                 res = output['PET']['RES']
-        elif action in self.API_OPS:
+        elif (action in self.API_OPS) or (action == 'INF'):
             if action == 'ACT_V2':
                 payload.update(
                     {'timefilter': self.TIMEFILTER, 'activityfilter': '0'})
@@ -82,14 +91,17 @@ class VerisureAPIClient():
 
     def generate_id(self):
         ID = 'IPH_________________________' + self.user + \
-            datetime.now().strftime("%Y%m%d%H%M%S")
+            datetime.now().strftime('%Y%m%d%H%M%S')
         return ID
 
     def get_login_hash(self):
         payload = self.LOGIN_PAYLOAD
         payload.update({'request': 'LOGIN', 'ID': self.generate_id()})
         output = self.call_verisure_get('POST', payload)
-        return output['PET']['HASH']
+        if output['PET']['RES'] == 'OK':
+            return output['PET']['HASH']
+        else:
+            return output
 
     def logout(self, hash):
         payload = self.OUT_PAYLOAD
@@ -100,15 +112,39 @@ class VerisureAPIClient():
 
     def operate_alarm(self, action):
         if (action in self.ALARM_OPS) or (action in self.API_OPS):
-            try:
-                hash = self.get_login_hash()
-                id = self.generate_id()
+            hash = self.get_login_hash()
+            if type(hash) is list:
+                return hash
+            id = self.generate_id()
+            if (action == 'IMG'):
+                if self.sensor == None:
+                    status = { 'RES': 'KO', 'MSG': 'Missing Sensor ID'}
+                    return status
+                self.instibs = self.op_verisure('SRV', hash, id)[
+                    'INSTALATION']['INSTIBS']
+                self.op_verisure(action, hash, id)
+                self.signaltype = '0'
+                while self.signaltype != '16':
+                    time.sleep(self.RATELIMIT)
+                    log = self.op_verisure('ACT_V2', hash, id)[
+                        'LIST']['REG'][0]
+                    self.idsignal = log['@idsignal']
+                    self.signaltype = log['@signaltype']
+                output = self.op_verisure('INF', hash, id)
+                for i in range(1, 4):
+                    f = open(datetime.now().strftime(
+                        '%Y%m%d%H%M%S') + '_' + str(i) + '.jpg', 'wb')
+                    f.write(base64.b64decode(
+                        output['DEVICES']['DEVICE']['IMG'][i - 1]['#text']))
+                    f.close()
+                    status = {'RES': 'OK', 'MSG': 'Images written to disk.'}
+            else:
                 status = self.op_verisure(action, hash, id)
-                self.logout(hash)
-                return status
-            except:
-                output = { 'RES':'ERROR', 'MSG':'Something went wrong. Check your credentials and/or the installation ID' }
-                return output
+            self.logout(hash)
+            return status
+        else:
+            status = { 'RES': 'KO', 'MSG': 'Invalid command.'}
+            return status
 
 
 def create_args_parser(help_cmd):
@@ -135,6 +171,10 @@ def create_args_parser(help_cmd):
                         '--language',
                         help='Your language (lowercase): es, it, fr, en, pt ...',
                         required=True)
+    parser.add_argument('-s',
+                        '--sensor',
+                        help='The sensor ID (to take a picture using IMG)',
+                        required=None)
     parser.add_argument('COMMAND',
                         help=textwrap.dedent(help_cmd),
                         type=str)
@@ -147,7 +187,7 @@ def main():
     args = create_args_parser(help_commands).parse_args()
     initparams = vars(args)
     client = VerisureAPIClient(**initparams)
-    output=client.operate_alarm(args.COMMAND)
+    output = client.operate_alarm(args.COMMAND)
     print(json.dumps(output, indent=2))
 
 
